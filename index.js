c21813c81456ed5a8aa899d243b95504a56f5c69
Richard Machado <mmachado.richard@gmail.com>
2023-12-13 14:27:12 -0800

    updating gitignore

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+require('dotenv').config();
+const accountSid = process.env.accountSID;
+const authToken = process.env.authTOKEN;
+//const PORT = process.env.PORT || 5000;
+
+const client = require('twilio')(accountSid, authToken);
+
+const server = require("./server.js");
+
+
+
+server.get('/api',(req,res) => {
+   client.messages
+    .create({
+        body: 'Hello from Twilio',
+        from: '+18886896605',
+        to: '+16199028420'
+    })
+    .then(message =>  res.send(`I'm here ${message.sid}`));
+   ;
+  });
+
+
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => { console.log(\n=== Server listening on port ${PORT} ===\n); });
+server.listen(PORT, () => { console.log(`\n=== Server listening on port ${PORT} ===\n`); });
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+
+
+server.get("/", (req, res) => {
+  res.send("<h1>server is running</h1>");
+});
+
+module.exports = server;
